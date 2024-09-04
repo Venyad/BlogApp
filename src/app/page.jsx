e886c602bec3ed8c7c6f25ed85a5e2f136983037
Venya <venyadurgam25@gmail.com>
2024-09-04 18:39:16 -0500

    Updated global css

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,8 @@
 import styles from "./homepage.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return <div>
+    <Link href="/" >Lorem ipsum dolor sit, amet consectetur adipisicing elit.</Link>
+    </div>;
 }
