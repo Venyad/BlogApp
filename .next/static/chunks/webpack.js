173484bb675a1f8d2f1a0af3a8eb59bec27f0c3b
Venya <venyadurgam25@gmail.com>
2024-09-04 19:54:28 -0500

    Updated global css

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "05aef7599bb391e9"; }
+/******/ 		__webpack_require__.h = function() { return "1356f70e43e1d404"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
