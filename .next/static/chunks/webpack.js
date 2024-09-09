007ce7f02c74f9ea2c903e6cbf67a0ab743fec1d
Venya <venyadurgam25@gmail.com>
2024-09-08 22:48:45 -0500

    Updated responsiveness in Navbar component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d79285ce638ac3fc"; }
+/******/ 		__webpack_require__.h = function() { return "7447bfcd7f81cec5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
