6102ab45e13c97ad9a92ee4648283ee150adf4fe
Venya <venyadurgam25@gmail.com>
2024-09-09 20:18:54 -0500

    Updated featured component responsiveness

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a3a03cb14b31833a"; }
+/******/ 		__webpack_require__.h = function() { return "216c4cb9c3fc8804"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
