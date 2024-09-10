5a661508d9205d08f41b5e65496790ec6f8d95c9
Venya <venyadurgam25@gmail.com>
2024-09-09 21:15:44 -0500

    Updated Menu  Component

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import styles from "./menu.module.css"
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Menu = () => {
   return (
     <div className={styles.container}>
-      Menu
+      <h2>{"What's hot"}</h2>
+      <h1>Most Popular</h1>
+      <div className={styles.items}>
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.travel}`}>Travel</span>
+            <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>John DOe</span>
+              <span className={styles.date}>10.03.2023</span>
+            </div>
+          </div>
+        </Link>
+      </div>
+
+
     </div>
   )
 }
