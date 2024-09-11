886ecfad65e6406ecfe4b70a582607ca69a1c0ed
Venya <venyadurgam25@gmail.com>
2024-09-10 23:39:22 -0500

    Updated Footer Component responsiveness

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5a8c6e40e4dbc3bd"; }
+/******/ 		__webpack_require__.h = function() { return "126799a9531cd9a5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
