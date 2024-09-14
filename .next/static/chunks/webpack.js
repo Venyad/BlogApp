0c2b776f6cbc3448690b766915bfbac78e76f17f
Venya <venyadurgam25@gmail.com>
2024-09-13 20:25:11 -0500

    Updated Single page component responsiveness

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c82151d1c50c0c41"; }
+/******/ 		__webpack_require__.h = function() { return "250d82255167c045"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
