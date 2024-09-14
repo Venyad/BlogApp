203d88f0e677a1b99f36ab47fb62b7f50a6c3233
Venya <venyadurgam25@gmail.com>
2024-09-13 20:13:55 -0500

    Updated Comments component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "50226aa66a512132"; }
+/******/ 		__webpack_require__.h = function() { return "cce39e4ce15f8601"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,12 +1,56 @@
 import React from 'react'
-import styles from "/comments.module.css"
+import styles from "./comments.module.css";
+import Link from 'next/link';
+import Image from "next/image";
+
+
+
 
 const Comments = () => {
-  return (
-    <div className={styles.container}>
-      
-    </div>
-  )
+
+    const status = "authenticated";
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>
+                Comments
+            </h1>
+            {status === "authenticated" ? (
+                <div className={styles.write}>
+                    <textarea
+                        placeholder="write a comment..."
+                        className={styles.input}
+                    />
+                    <button className={styles.button}>
+                        Send
+                    </button>
+                </div>
+            ) : (
+                <Link href="/login">Login to write a comment</Link>
+            )}
+            <div className={styles.comments}>
+                <div className={styles.comment}>
+                    <div className={styles.user}>
+                        <Image
+                            src="/p1.jpeg"
+                            alt=""
+                            width={50}
+                            height={50}
+                            className={styles.image}
+                        />
+
+                        <div className={styles.userInfo}>
+                            <span className={styles.username}>John Doe</span>
+                            <span className={styles.date}>01.01.2023</span>
+                        </div>
+                    </div>
+                    <p className={styles.desc}>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, sed ipsa quam maiores illum unde atque nemo nostrum temporibus voluptate ut ipsam soluta, dolorem impedit eaque ad? Eum, aliquid dolor.
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
-export default Comments
+export default Comments;
