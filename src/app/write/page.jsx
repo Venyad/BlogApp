0446d8b686f067de6de63ed0990f83f9e52195e6
Venya <venyadurgam25@gmail.com>
2024-09-15 18:24:51 -0500

    Created write component

---
+++ src/app/write/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./writePage.module.css"
+
+const WritePage = () => {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
+
+export default WritePage
