8ff195b660113ea4b9599f51465702c0ac31c01b
Venya <venyadurgam25@gmail.com>
2024-09-15 18:32:20 -0500

    Updated writepage component

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import Image from 'next/image'
+import React, { useState } from 'react'
 import styles from "./writePage.module.css"
 
 const WritePage = () => {
-  return (
-    <div className={styles.container}>
-      
-    </div>
-  )
+
+    const [open, setOpen] = useState(false);
+    ; return (
+        <div className={styles.container}>
+            <input type="text" placeholder="Title" />
+            <div className={styles.editor}>
+                <button className={styles.button}>
+                    <Image src="/plus.png" alt="" width={16} height={16} />
+                </button>
+                {open && (
+                    <div className={styles.add}>
+                        <button className={styles.addButton}>
+                            <Image src="/image.png" alt="" width={16} height={16} />
+                        </button>
+                        <button className={styles.addButton}>
+                            <Image src="/external.png" alt="" width={16} height={16} />
+                        </button>
+                        <button className={styles.addButton}>
+                            <Image src="/video.png" alt="" width={16} height={16} />
+                        </button>
+                    </div>
+                )}
+            </div>
+        </div>
+    )
 }
 
 export default WritePage
