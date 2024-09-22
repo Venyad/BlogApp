1b3a80734162113ab9b8e2a0cb8a9421b14dfe13
Venya <venyadurgam25@gmail.com>
2024-09-21 23:35:17 -0500

    Updated page file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0ac96a02907a805e"; }
+/******/ 		__webpack_require__.h = function() { return "0e64acb419198155"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
