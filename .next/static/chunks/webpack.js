138a8bf8f2c3b19e847622c77a24df4ac41f1091
Venya <venyadurgam25@gmail.com>
2024-09-23 23:34:14 -0500

    Updated pagination  component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b22593cebbc59e9b"; }
+/******/ 		__webpack_require__.h = function() { return "83a60fccc33382bd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
