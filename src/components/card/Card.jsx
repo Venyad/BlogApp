dc968c264960fd37213e89ba3cb2c3bcbc6f5034
Venya <venyadurgam25@gmail.com>
2024-10-04 18:04:09 -0500

    Updated Card component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,16 +6,16 @@ import Link from 'next/link';
 const Card = ({key,item}) => {
     return (
         <div className={styles.container} key={key}>
-            <div className={styles.imageContainer}>
-                {item.img && <Image src={item.img} alt="" fill className={styles.image}/>}
-            </div>
+            {item.img && <div className={styles.imageContainer}>
+                <Image src={item.img} alt="" fill className={styles.image}/>
+            </div>}
             <div className={styles.textContainer}>
                 <div className={styles.detail}>
                     <span className={styles.date}>{item.createdAt.substring(0,10)} - {""} </span>
                     <span className={styles.category}> {item.catSlug}</span>
 
                 </div>
-                <Link href="/">
+                <Link href={`/posts/${item.slug}`}>
                     <h1>{item.title}</h1>
 
                 </Link>
