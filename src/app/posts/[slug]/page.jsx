2d5a12c423426598f99274b77386d93282e9daff
Venya <venyadurgam25@gmail.com>
2024-10-04 18:15:04 -0500

    Updated git app page content

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -4,16 +4,31 @@ import Image from 'next/image'
 import Menu from '@/components/menu/Menu'
 import Comments from '@/components/comments/Comments'
 
-const SinglePage = () => {
+
+const getData = async (slug) => {
+    const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+      cache: "no-store",
+    });
+  
+    if (!res.ok) {
+      throw new Error("Failed");
+    }
+  
+    return res.json();
+  };
+const SinglePage = async ({params}) => {
+    const {slug} = params;
+    const data = await getData(slug);
     return (
         <div className={styles.container}>
             <div className={styles.infoContainer}>
                 <div className={styles.textContainer}>
-                    <h1 className={styles.title}>Lorem ipsum dolor sit amet consectetur</h1>
+                    <h1 className={styles.title}>{data?.title}</h1>
                     <div className={styles.user}>
-                        <div className={styles.userImageContainer}>
-                            <Image src="/p1.jpeg" alt="" fill className={styles.avatar} />
+                       {data?.user?.image && <div className={styles.userImageContainer}>
+                            <Image src={data.user.image} alt="" fill className={styles.avatar} />
                         </div>
+                        }
                         <div className={styles.userTextContainer}>
                             <span className={styles.username}>John Doe</span>
                             <span className={styles.date}>01.01.2024</span>
@@ -21,18 +36,12 @@ const SinglePage = () => {
                     </div>
                 </div>
                 <div className={styles.imageContainer}>
-                    <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+                    <Image src={data.img} alt="" fill className={styles.image} />
                 </div>
             </div>
             <div className={styles.content}>
                 <div className={styles.post}>
-                    <div className={styles.description}>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt error fugit praesentium quisquam dolores labore facilis, consectetur voluptatum ex quos nihil vel doloremque. Iure iste animi, omnis vitae eum debitis!</p>
-                    <h2>Lorem, ipsum dolor sit amet consectetur</h2>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt error fugit praesentium quisquam dolores labore facilis, consectetur voluptatum ex quos nihil vel doloremque. Iure iste animi, omnis vitae eum debitis!</p>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt error fugit praesentium quisquam dolores labore facilis, consectetur voluptatum ex quos nihil vel doloremque. Iure iste animi, omnis vitae eum debitis!</p>
-
-                    </div>
+                    <div className={styles.description} dangerouslySetInnerHTML={{ __html: data?.desc }}/>
                     <div className={styles.comment}>
                         <Comments/>
                     </div>
@@ -43,4 +52,4 @@ const SinglePage = () => {
     )
 }
 
-export default SinglePage
+export default SinglePage;
